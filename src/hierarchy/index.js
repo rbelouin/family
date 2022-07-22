293d918059bebadc2c3689831869138a8b7c490a
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2022-07-22 23:58:44 +0200

    Remove drag functionality

--- src/hierarchy/index.js
+++ src/hierarchy/index.js
@@ -380,30 +380,6 @@ const data = {
  ]
 }
 
-const drag = simulation => {
-  function dragstarted(event, d) {
-    if (!event.active) simulation.alphaTarget(0.3).restart();
-    d.fx = d.x;
-    d.fy = d.y;
-  }
-  
-  function dragged(event, d) {
-    d.fx = event.x;
-    d.fy = event.y;
-  }
-  
-  function dragended(event, d) {
-    if (!event.active) simulation.alphaTarget(0);
-    d.fx = null;
-    d.fy = null;
-  }
-  
-  return d3.drag()
-      .on("start", dragstarted)
-      .on("drag", dragged)
-      .on("end", dragended);
-};
-
 const width = 600;
 const height = 600;
 
@@ -436,8 +412,7 @@ const node = svg.append("g")
   .join("circle")
     .attr("fill", d => d.children ? null : "#000")
     .attr("stroke", d => d.children ? null : "#fff")
-    .attr("r", 3.5)
-    .call(drag(simulation));
+    .attr("r", 3.5);
 
 node.append("title")
     .text(d => d.data.name);
