5bb1051fa31ac5d5b2fd99582fbe941fb4e11ff6
Rodolphe Belouin <rodolphe.belouin@gmail.com>
2022-07-23 02:28:20 +0200

    Organize nodes in circles

--- src/hierarchy/index.ts
+++ src/hierarchy/index.ts
@@ -4,14 +4,17 @@ import data from "./data.json";
 
 // Source: https://observablehq.com/@d3/force-directed-tree
 
-const width = 900;
-const height = 900;
+const width = 1000;
+const height = 1000;
+const distance = 30;
 
 type Datum = { name: string; value?: number; children?: Datum[] };
 type Node = HierarchyNode<Datum> & SimulationNodeDatum;
 type Link = HierarchyLink<Datum> & SimulationLinkDatum<Node>;
 
 const root = d3.hierarchy(data) as Node;
+organizeNodes(root);
+
 const links = root.links() as Link[];
 const nodes = root.descendants();
 
@@ -43,7 +46,17 @@ const node = svg.append("g")
     .attr("r", 3.5);
 
 node.append("title")
-    .text(d => d.data.name);
+    .text(d => `${d.data.name} - ${d.x},${d.y}`);
+
+node
+    .attr("cx", d => d.x || null)
+    .attr("cy", d => d.y || null);
+
+link
+    .attr("x1", d => (d.source as Node).x || null)
+    .attr("y1", d => (d.source as Node).y || null)
+    .attr("x2", d => (d.target as Node).x || null)
+    .attr("y2", d => (d.target as Node).y || null);
 
 simulation.on("tick", () => {
   link
@@ -60,4 +73,30 @@ simulation.on("tick", () => {
 const chart = svg.node();
 if (chart) {
   document.querySelector("main")?.appendChild(chart);
+}
+
+function organizeNodes(rootNode: Node) {
+  root.sum(() => 1);
+
+  function organizeNode(node: Node, layer: number, range: [number, number]) {
+    const [x, y] = d3.pointRadial((range[0] + range[1]) / 2, distance * layer);
+    node.x = x;
+    node.y = y;
+
+    const children = node.children || [];
+
+    const sectionAngle = range[1] - range[0];
+    let angleStart = range[0];
+    children.forEach((child) => {
+      const ratio = node.value && child.value
+        ? child.value / node.value
+        : 1 / children.length;
+      const angleEnd = angleStart + ratio * sectionAngle;
+      organizeNode(child, layer + 1, [angleStart, angleEnd]);
+
+      angleStart = angleEnd;
+    });
+  }
+
+  organizeNode(rootNode, 0, [0, 2*Math.PI]);
 }
